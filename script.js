fe013e460b5338307fe17115e75d07b58a50e7a2
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-07 20:10:21 -0400

    js to clean search input text with string replace regex

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ $(document).ready(function(){
     });
   }
 
+  $("#submit").click(function(){
+    console.log("you pressed search");
+    var srchtext = $("#search").val();
+    console.log(srchtext);
+    //replace spaces with '+' and remove punctuation
+    var cleantext = srchtext.toLowerCase().replace(/\s/g,'+').replace(/[^\w\+]/g,'');
+    console.log(cleantext);
+  })
+
+  //need to keep page from reloading when you press enter in search box
+  //how do html forms work when you 'submit', how to use AJAX instead of
+  //default HTML page reload behavior
+
+
 });
