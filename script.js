819c023ec59e900d2ccaa434871d7297907d89c7
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-08 06:52:11 -0400

    add warning text behavior for empty search box

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 $(document).ready(function(){
-
-
+  //hide warning elements on page load
+  $(".warn").hide();
+  //declare function for API call
   function wikipediaCall(){
     basestring = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Cextracts&indexpageids=1&iwurl=1&inprop=url%7Cdisplaytitle&exchars=140&exintro=1"
     srchrequest = "titles="+$("#search").text();
     qrystring = basestring + srchrequest;
     $.getJSON(qrystring,function(result){
-      console.log(result);
+      //console.log(result);
+      //do something with returned API data
     });
   }
 
-  $("#submit").click(function(){
-    console.log("you pressed search");
+  $("#srchform").submit(function(event){
+    //stop browser from submitting the form
+    event.preventDefault();
+    //hide warnings again if present
+    $(".warn").hide();
+
     var srchtext = $("#search").val();
-    console.log(srchtext);
-    //replace spaces with '+' and remove punctuation
-    var cleantext = srchtext.toLowerCase().replace(/\s/g,'+').replace(/[^\w\+]/g,'');
-    console.log(cleantext);
+
+    //add message when search field is blank
+    if(srchtext == ""){
+      $(".warn").show();
+    }else{
+      //grab search value replace spaces with '+' and remove punctuation
+      srchtext = srchtext.replace(/\s/g,'+').replace(/[^\w\+]/g,'');
+      console.log("search query = " + srchtext);
+    }
+
+
+
+
+    //execute API call with search string
   })
-
-  //need to keep page from reloading when you press enter in search box
-  //how do html forms work when you 'submit', how to use AJAX instead of
-  //default HTML page reload behavior
-
 
 });
