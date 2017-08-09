b5e52cb459c8bd817271cdaae588e6fe6b5d8326
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-09 07:03:14 -0400

    functioning API call and draft of jquery DOM mods for results

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ $(document).ready(function(){
   //hide warning elements on page load
   $(".warn").hide();
   //declare function for API call
-  function wikipediaCall(){
-    basestring = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Cextracts&iwurl=1&inprop=url%7Cdisplaytitle&exchars=140&exintro=1"
-    srchrequest = "titles="+$("#search").text();
-    qrystring = basestring + srchrequest;
+  function wikipediaCall(text){
+    basestring = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&titles=";
+    options = "&prop=info%7Cextracts&iwurl=1&inprop=url%7Cdisplaytitle&exchars=140&exintro=1";
+    // str2 = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=info&titles=words&inprop=url";
+    qrystring = basestring + text + options;
+    // qrystring = str2;
     $.getJSON(qrystring,function(result){
       //console.log(result);
       //do something with returned API data
         //grab query.pages
-        console.log(results.query.pages[0]);
+        console.log(result.query.pages);
+        pages = result.query.pages;
+        console.log(pages[0]); //can't call object by index, need name...but won't know names ahead of time...
         //for each page
-          //add div container
-          //write page title
-          //link to page URL
-          //write extract below title
+          //add div container with title, URL, and extract
+          // content = "<div class=result><a target='_blank' href=" + pages.fullurl + ">" + pages.displaytitle + "</a>" + pages.extract + "</div>";
+          // $("footer").before(content);
+
+
     });
   }
 
@@ -34,6 +39,7 @@ $(document).ready(function(){
       //grab search value replace spaces with '+' and remove punctuation
       srchtext = srchtext.replace(/\s/g,'+').replace(/[^\w\+]/g,'');
       console.log("search query = " + srchtext);
+      wikipediaCall(srchtext);
     }
 
 
